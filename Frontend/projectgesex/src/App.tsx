71614efdc9b2da327b3a5a40fd832453f7a8336e
Sebastian <sebaas.tiang10@gmail.com>
2025-04-07 16:03:29 -0500

    Se agrego la parte principal

--- Frontend/projectgesex/src/App.tsx
+++ Frontend/projectgesex/src/App.tsx
@@ -1,19 +1,24 @@
-
-
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import QuestionCard from "./components/QuestionCard";
 
 function App() {
-
-
   return (
-    <>
-      <div className="bg-red-500 p-10 text-white text-3xl decoration-sky-500">
-        Tailwind está funcionando?
-      </div>
-      <div>
-        <a className="underline decoration-sky-500 text-right">My Company, Inc</a>
-      </div>
-    </>
-  )
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+
+      <main className="flex-grow p-8 bg-gray-50">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 place-items-center">
+          <QuestionCard question="1. ¿Cuál es tu opinión sobre la diversidad sexual?" />
+          <QuestionCard question="1. ¿Cuál es tu opinión sobre la diversidad sexual?" />
+          <QuestionCard question="1. ¿Cuál es tu opinión sobre la diversidad sexual?" />
+          <QuestionCard question="1. ¿Cuál es tu opinión sobre la diversidad sexual?" />
+        </div>
+      </main>
+
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
