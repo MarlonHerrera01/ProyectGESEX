48ba3dd3ad971d84bf454a34a138c41fb7dc80b7
Sebastian <sebaas.tiang10@gmail.com>
2025-04-10 16:44:43 -0500

    Se modifico el componente del Navbar para que funcione redirigiendo a las paginas

--- Frontend/projectgesex/src/components/Navbar.tsx
+++ Frontend/projectgesex/src/components/Navbar.tsx
@@ -1,13 +1,16 @@
 import { User } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="bg-red-700 text-white px-6 py-4 flex justify-between items-center">
-      <img src="/logo.png" alt="Logo" className="h-16" />
+      <Link to="/">
+        <img src="/logo.png" alt="Logo" className="h-16" />
+      </Link>
       <ul className="flex gap-6 text-lg">
-        <li><a href="#" className="hover:underline">Material de apoyo</a></li>
-        <li><a href="#" className="hover:underline">Encuesta</a></li>
-        <li><a href="#" className="hover:underline">Estadísticas</a></li>
+        <li><Link to="/encuesta" className="hover:underline">Encuesta</Link></li>
+        <li><Link to="/estadisticas" className="hover:underline">Estadísticas</Link></li>
+        <li><Link to="/material" className="hover:underline">Material de apoyo</Link></li>
       </ul>
       <User className="w-12 h-12" />
     </nav>
