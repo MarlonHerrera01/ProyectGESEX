40b2f8a0ade45c8f6453a3d01dc4765b057e0ef7
Sebastian <sebaas.tiang10@gmail.com>
2025-04-12 15:50:01 -0500

    Se modifico las proporciones de los elementos del navbar

--- Frontend/projectgesex/src/components/Navbar.tsx
+++ Frontend/projectgesex/src/components/Navbar.tsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="bg-red-700 text-white px-6 py-4 flex justify-between items-center">
+    <nav className="bg-red-700 text-white px-6 py-2 flex justify-between items-center">
       <Link to="/">
-        <img src="/logo.png" alt="Logo" className="h-16" />
+        <img src="/logo.png" alt="Logo" className="h-20" />
       </Link>
-      <ul className="flex gap-6 text-lg">
+      <ul className="flex gap-6 text-lg"> {/* text-lg para que no se vea tan grande */}
         <li><Link to="/encuesta" className="hover:underline">Encuesta</Link></li>
         <li><Link to="/estadisticas" className="hover:underline">Estadísticas</Link></li>
         <li><Link to="/material" className="hover:underline">Material de apoyo</Link></li>
